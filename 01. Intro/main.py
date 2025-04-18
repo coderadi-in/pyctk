b3625ea9cc1b47b1d6d8782b1805f413e4e80b8d
Aditya <adiprofitcoder@gmail.com>
2025-04-18 11:40:23 +0530

    Add files via upload

---
+++ 01. Intro/main.py
@@ -0,0 +1,19 @@
+'''PyCODE | @_py.code'''
+
+# $ pip install customtkinter --upgrade
+
+# | Window
+
+# ? Importing Libraries
+from customtkinter import *
+
+# ! The Root Window
+root = CTk() # Main Window
+
+# Set appearance mode
+set_appearance_mode('dark')
+
+# ! Run the UI
+root.mainloop()
+
+# | source code at github
