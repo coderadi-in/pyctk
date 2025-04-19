cd778dd92040d8b5cf07a7ac47028d72c8f4d80a
Aditya <adiprofitcoder@gmail.com>
2025-04-19 10:51:27 +0530

    Add files via upload

--- 01. Intro/main.py
+++ 01. Intro/main.py
@@ -10,8 +10,25 @@ from customtkinter import *
 # ! The Root Window
 root = CTk() # Main Window
 
-# Set appearance mode
-set_appearance_mode('dark')
+# | Appearance Mode (Theme)
+set_appearance_mode('system')
+
+# | Sizing
+geomtry = "600x400+100+100"
+# geomtry = "{width}x{height}"
+root.geometry(geomtry)
+
+# | Title
+root.title("My App")
+
+# | Icon
+root.iconbitmap('icon.ico')
+
+# | State
+root.state('zoomed')
+
+# | Color
+root.configure(fg_color='#FF0000')
 
 # ! Run the UI
 root.mainloop()
