f809e73af88ae9a11dec78abf0b30343c3b2545d
Aditya <adiprofitcoder@gmail.com>
2025-04-21 10:41:47 +0530

    Add files via upload

--- 02. Basic Widgets/main.py
+++ 02. Basic Widgets/main.py
@@ -10,7 +10,11 @@ root.geometry("600x400")
 
 # | Label
 label = CTkLabel(
-    root, text="Hello world"
+    root, text="Hello world", text_color="#0000ff",
+    fg_color="#00ff00",
+    bg_color="#000000",
+    corner_radius=10,
+    font=("Arial", 20, "bold")
 )
 label.pack()
 
